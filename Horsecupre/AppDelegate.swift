af5deb9d110b6ea20e4ddc0ded7c1d85fb6e2118
mumulin1117 <mumulin1117@163.com>
2025-09-10 16:33:16 +0800

    项目初始化

---
+++ Horsecupre/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Horsecupre
+//
+//  Created by mumu on 2025/9/10.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+    
+}
+
---
+++ Pods/Target Support Files/Pods-Horsecupre/Pods-Horsecupre-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Horsecupre : NSObject
+@end
+@implementation PodsDummy_Pods_Horsecupre
+@end
